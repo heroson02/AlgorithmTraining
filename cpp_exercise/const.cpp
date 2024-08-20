73377b35b7d0ecda772a220e4092e30f2925cc1b
Son_Hero <heroson0925@gmail.com>
2024-08-20 17:27:05 +0900

    ADD cpp_exercise

---
+++ cpp_exercise/const.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	const int BUFSIZE = 10;
+
+	//const를 사용하면 해당 변수를 상수로 설정한다.
+}
+
+//C에서는 정해진 값을 선언할 때 define을 사용했다. 그러나 C++에서는 define의 자리를 const가 대신한다.
+//define에 비해서 변수형을 지정할 수 있어서 실수를 방지할 수 있다.
